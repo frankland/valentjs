82a5df5c978bd7658883e663bf8f22866a535fcf
Valeriy <valeriy.sorokobatko@gmail.com>
2015-05-02 20:38:41 +0300

    add diggest component

---
+++ src/components/diggest.js
@@ -0,0 +1,26 @@
+import Scope from './scope';
+import Injector from './injector';
+
+function safe(scope, fn) {
+  var phase = scope.$root.$$phase;
+  if(phase == '$apply' || phase == '$digest') {
+    if(angular.isFunction(fn)) {
+      fn();
+    }
+  } else {
+    scope.$apply(fn);
+  }
+}
+
+export default class Diggest {
+  static run(context, fn = () => {}) {
+    if (context) {
+      Scope.get(context).then((scope) => {
+        safe(scope, fn);
+      });
+    } else {
+      var scope = Injector.get('$rootScope');
+      safe(scope, fn);
+    }
+  }
+}
