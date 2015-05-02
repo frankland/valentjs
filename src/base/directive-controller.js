791f7c2487e6553ab0491679353dabc06942c02f
Valeriy <valeriy.sorokobatko@gmail.com>
2015-05-02 20:39:16 +0300

    remove uneeded wrappers and base components

--- src/base/directive-controller.js
+++
@@ -1,9 +0,0 @@
-import Controller from './controller';
-
-export default class DirectiveController extends Controller {
-
-  constructor(scope) {
-    super(scope);
-    this.pipes = this.scope.pipes;
-  }
-}
