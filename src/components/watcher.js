22d480533a50e76ce616a6392344789f9d525a6a
Valeriy <valeriy.sorokobatko@gmail.com>
2015-05-02 21:10:38 +0300

    update watcher class

--- src/components/watcher.js
+++ src/components/watcher.js
@@ -1,33 +1,33 @@
 import Scope from './scope';
 
-var privateScope = Symbol('$scope');
+var scope = Symbol('$scope');
 
 /**
  * TODO: auto add "controller" to watch conditions
  */
 export default class Watcher {
   static create(context) {
-    return Scope.get(context).then((scope) => {
-      return new Watcher(scope);
+    return Scope.get(context).then(($scope) => {
+      return new Watcher($scope);
     });
   }
 
-  constructor(scope) {
-    this[privateScope] = scope;
+  constructor($scope) {
+    this[scope] = $scope;
   }
 
   watch() {
-    var scope = this[privateScope];
-    return scope.$watch.apply(scope, arguments);
+    var $scope = this[scope];
+    return $scope.$watch.apply($scope, arguments);
   }
 
   watchGroup() {
-    var scope = this[privateScope];
-    return scope.$watchGroup.apply(scope, arguments);
+    var $scope = this[scope];
+    return $scope.$watchGroup.apply($scope, arguments);
   }
 
   watchCollection() {
-    var scope = this[privateScope];
-    return scope.$watchCollection.apply(scope, arguments);
+    var $scope = this[scope];
+    return $scope.$watchCollection.apply($scope, arguments);
   }
 }
