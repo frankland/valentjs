a140ecd996db1a95da263d2981f113554534754e
Valerii Sorokobatko <valeriy.sorokobatko@gmail.com>
2015-10-23 14:43:42 +0300

    remove tests

--- test/index.js
+++ test/index.js
@@ -1,19 +1,19 @@
-import './utils/object-description';
-import './utils/object-transition';
+//import './utils/object-description';
+//import './utils/object-transition';
+////
+//import './components/serializer';
+//// import './components/url';
+//import './components/url-struct';
+//import './components/url';
+////
+//import './manager';
 //
-import './components/serializer';
-// import './components/url';
-import './components/url-struct';
-import './components/url';
 //
-import './manager';
-
-
-import './route/route-config';
-import './route/route-flow';
-import './route/route-convert';
-import './route/route-url-struct';
-
-
-import './controller/controller-flow';
-import './controller/controller-model';
+//import './route/route-config';
+//import './route/route-flow';
+//import './route/route-convert';
+//import './route/route-url-struct';
+//
+//
+//import './controller/controller-flow';
+//import './controller/controller-model';
