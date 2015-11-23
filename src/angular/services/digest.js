6004d0f1fecf52c270e65c0520d042f5e388295b
Valerii Sorokobatko <valeriy.sorokobatko@gmail.com>
2015-11-23 22:32:25 +0200

    Digest utility - add debounce for diggest function - now "digest" is a function instaed of class with static method

--- src/angular/services/digest.js
+++ src/angular/services/digest.js
@@ -1,3 +1,5 @@
+import debounce from 'lodash/function/debounce';
+
 import Scope from './scope';
 import Injector from './injector';
 
@@ -16,17 +18,27 @@ function safe(scope, fn) {
   }
 }
 
-export default class Digest {
-  static run(context, fn) {
-    if (context) {
-      if (Scope.has(context)) {
-        Scope.get(context).then((scope) => {
-          safe(scope, fn);
-        });
-      }
-    } else {
-      let scope = Injector.get('$rootScope');
-      safe(scope, fn);
+let digest = (context, fn) => {
+  if (context) {
+    if (Scope.has(context)) {
+      Scope.get(context).then((scope) => {
+        safe(scope, fn);
+      });
     }
+  } else {
+    let scope = Injector.get('$rootScope');
+    safe(scope, fn);
   }
-}
+};
+
+/**
+ * TODO: add possibility to configure debounce options
+ */
+const timeout = 50;
+
+const options = {
+  leading: false,
+  trailing: true
+};
+
+export default debounce(digest, timeout, options);
