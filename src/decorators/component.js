8e32db573957507a896398f43bac42b7d9f8d807
balkon_smoke <balkon.smoke@me.com>
2015-11-30 13:59:41 +0200

    fix module name

--- src/decorators/component.js
+++ src/decorators/component.js
@@ -1,4 +1,4 @@
-import { getControllerName } from '../utils/classname';
+import { getControllerName } from '../utils/class-name';
 
 export default options => target => {
   let controllerName = getControllerName(target);
