975b0645c91a7ea2f555fa60c6917a1282c69e99
Valerii Sorokobatko <valeriy.sorokobatko@gmail.com>
2015-12-01 03:30:43 +0200

    first steps to implement base controllers

---
+++ src/angular/base/screen-controller.js
@@ -0,0 +1,32 @@
+import Injector from '../services/injector';
+import digest from '../services/digest';
+
+
+let proxy = (name) => {
+  console.info(`"${name}" could be implemented in child class`);
+};
+
+export default class BaseScreenController {
+
+  digest = () => digest(this);
+
+  injector = Injector;
+
+  constructor(url) {
+    this.url = url;
+
+    this.createUrlLinks(url);
+
+    this.url.watch((params, diff, options) => {
+      this.onUrlChange(params, diff, options);
+    });
+  }
+
+  onUrlChange(params, diff, options) {
+    proxy('onUrlChange');
+  }
+
+  createUrlLinks(url) {
+    proxy('createUrlLinks');
+  }
+}
